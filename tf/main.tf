16355c1e6a5177343d362851f8bcf6f7c06dfd9e
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-06-27 00:00:26 +0100

    Update main.tf to use var instead of vars for environment variable exports

--- tf/main.tf
+++ tf/main.tf
@@ -93,12 +93,12 @@ resource "aws_instance" "runner" {
   }
   provisioner "remote-exec" {
     inline = [
-      "export AUTH0_SECRET=${vars.AUTH0_SECRET}",
-      "export AUTH0_CLIENT_ID=${vars.AUTH0_CLIENT_ID}",
-      "export AUTH0_CLIENT_SECRET=${vars.AUTH0_CLIENT_SECRET}",
-      "export AUTH0_ISSUER_BASE_URL=${vars.AUTH0_ISSUER_BASE_URL}",
-      "export MONGO_URI=${vars.MONGO_URI}",
-      "export AUTH0_BASE_URL=${vars.AUTH0_BASE_URL}",
+      "export AUTH0_SECRET=${var.AUTH0_SECRET}",
+      "export AUTH0_CLIENT_ID=${var.AUTH0_CLIENT_ID}",
+      "export AUTH0_CLIENT_SECRET=${var.AUTH0_CLIENT_SECRET}",
+      "export AUTH0_ISSUER_BASE_URL=${var.AUTH0_ISSUER_BASE_URL}",
+      "export MONGO_URI=${var.MONGO_URI}",
+      "export AUTH0_BASE_URL=${var.AUTH0_BASE_URL}",
       "git clone https://github.com/nmcc1212/final-tsi-project.git",
       "cd final-tsi-project/tf",
       "docker compose up",
